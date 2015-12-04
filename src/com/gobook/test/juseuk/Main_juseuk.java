13883c21605e4810eac66d63b7529f26289269b3
gobookgobook <gobookgobook@naver.com>
2015-12-04 16:22:07 +0900

    2015_12_04_16:21 오주석:추가

---
+++ src/com/gobook/test/juseuk/Main_juseuk.java
@@ -0,0 +1,10 @@
+package com.gobook.test.juseuk;
+
+public class Main_juseuk {
+	public static void main(String[] args) {
+		
+		Sub_juseuk a=new Sub_juseuk();		
+		a.disp();
+		System.out.println("어렵다.");
+	}
+}
---
+++ src/com/gobook/test/juseuk/Sub_juseuk.java
@@ -0,0 +1,9 @@
+package com.gobook.test.juseuk;
+
+public class Sub_juseuk {
+
+	public void disp(){
+		System.out.println("힘들엉");
+	}
+
+}
