6a8237a49a629272feb2d09a73b91b5d6c33ea35
gobookgobook <gobookgobook@nate.com>
2015-12-04 16:08:02 +0900

    2015_12_04_16:07 성기훈 gobookDto

---
+++ src/com/gobook/test/Test_kihoon.java
@@ -0,0 +1,15 @@
+package com.gobook.test;
+
+public class Test_kihoon {
+	// 한글테스트입니다
+	private String gobook;
+
+	public String getGobook() {
+		return gobook;
+	}
+
+	public void setGobook(String gobook) {
+		this.gobook = gobook;
+	}
+	
+}
