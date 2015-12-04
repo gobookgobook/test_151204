f25a9e012cbb222507aee60001a56f513664c733
gobook <gobookgobook@naver.com>
2015-12-04 16:20:20 +0900

    2015_12_04_16:20 김재석 : kimjaeseokgod

---
+++ src/com/gobook/test/kimjaeseok/Test_kimjaeseokgod.java
@@ -0,0 +1,10 @@
+package com.gobook.test.kimjaeseok;
+
+public class Test_kimjaeseokgod {
+
+	public static void main(String[] args) {
+		System.out.println("오늘은 칼퇴하는날");
+        System.out.println("룰루랄라");
+	}
+
+}
---
+++ src/com/gobook/test/kimjaeseok/Test_main.java
@@ -0,0 +1,10 @@
+package com.gobook.test.kimjaeseok;
+
+public class Test_main {
+
+	public static void main(String[] args) {
+		System.out.println("오주석=목업장인");
+
+	}
+
+}
