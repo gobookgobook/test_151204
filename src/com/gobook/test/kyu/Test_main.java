026e254f864fb5e9ef3895ca2c7b3fb65daba488
gobook <gobookgobook@naver.com>
2015-12-04 16:25:14 +0900

    2015_12_04_16:24 황규성 : 클래스 추가

---
+++ src/com/gobook/test/kyu/Test_main.java
@@ -0,0 +1,9 @@
+package com.gobook.test.kyu;
+
+public class Test_main {
+
+	public static void main(String[] args) {
+		Test_number t=new Test_number();
+		t.disp();
+	}
+}
---
+++ src/com/gobook/test/kyu/Test_number.java
@@ -0,0 +1,7 @@
+package com.gobook.test.kyu;
+
+public class Test_number {
+	public void disp(){
+		System.out.println("12/4(금) - 칼퇴근");
+	}
+}
