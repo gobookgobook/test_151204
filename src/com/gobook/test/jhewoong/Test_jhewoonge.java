9d1cdaab256fb0b897f53b75ae67f2dcbc60a192
gobookgobook <gobookgobook@naver.com>
2015-12-04 16:29:05 +0900

    2015_12_04_16:28 조재웅 : 패키지및 클래스

---
+++ src/com/gobook/test/jhewoong/Test_jhewoonge.java
@@ -0,0 +1,8 @@
+package com.gobook.test.jhewoong;
+
+public class Test_jhewoonge {
+	public static void main(String[] args) {
+	Test_jhewoongs tj=new Test_jhewoongs();
+	tj.disp();
+	}
+}
---
+++ src/com/gobook/test/jhewoong/Test_jhewoongs.java
@@ -0,0 +1,7 @@
+package com.gobook.test.jhewoong;
+
+public class Test_jhewoongs {
+		public void disp(){
+			System.out.println("아흐..");
+		}	
+}
