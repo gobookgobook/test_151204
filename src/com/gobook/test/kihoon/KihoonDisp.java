669b45d467f8ec94ec2683a855eded2850b3ae6f
gobookgobook <gobookgobook@nate.com>
2015-12-04 16:17:44 +0900

    2015_12_04_16:17 성기훈 디스프함수

---
+++ src/com/gobook/test/kihoon/KihoonDisp.java
@@ -0,0 +1,7 @@
+package com.gobook.test.kihoon;
+
+public class KihoonDisp {
+	public void disp(){
+		System.out.println("기훈클래스 출력입니다.");
+	}
+}
---
+++ src/com/gobook/test/kihoon/KihoonMain.java
@@ -0,0 +1,8 @@
+package com.gobook.test.kihoon;
+
+public class KihoonMain {
+	public static void main(String[] args) {
+		KihoonDisp kd=new KihoonDisp();
+		kd.disp();
+	}
+}
