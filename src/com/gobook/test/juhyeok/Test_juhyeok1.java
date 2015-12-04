3ee2659599a96ece68e171996524c952427ccb41
gobookgobook <gobookgobook@naver.com>
2015-12-04 16:31:46 +0900

    2015_12_04_16:31 강주혁 : 주혁패키지

---
+++ src/com/gobook/test/juhyeok/Test_juhyeok1.java
@@ -0,0 +1,17 @@
+package com.gobook.test.juhyeok;
+
+public class Test_juhyeok1 {
+
+	public static void main(String[] args) {
+		Test_juhyeok2 tj = new Test_juhyeok2();
+		
+		tj.setX(10);
+		tj.setY(20);
+		
+		tj.toString();
+		//한글 한글
+		
+		System.out.println("한글갑니다.");
+	}
+
+}
